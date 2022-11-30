a6b1a59fc09b63777c782dcf8c40fcaa18242d73
IraMatsuk <g3726866@gmail.com>
2022-11-30 13:55:41 +0500

    change the test class for certificate repository

--- persistence/src/test/java/com/epam/esm/repository/GiftCertificateRepositoryTest.java
+++ persistence/src/test/java/com/epam/esm/repository/GiftCertificateRepositoryTest.java
@@ -1,60 +1,36 @@
 //package com.epam.esm.repository;
 //
 //import com.epam.esm.entity.GiftCertificate;
-//import com.epam.esm.entity.Tag;
 //import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 //
-//import java.math.BigDecimal;
-//import java.time.LocalDateTime;
 //import java.util.List;
-//import java.util.Set;
+//import java.util.Optional;
 //
+//import static org.assertj.core.api.Assertions.assertThat;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
 //
-//@ExtendWith(SpringExtension.class)
+//@RunWith(SpringRunner.class)
 //@DataJpaTest
 //public class GiftCertificateRepositoryTest {
-//    private final GiftCertificate giftCertificateOne = GiftCertificate.builder()
-//                .id(1L)
-//                .name("certificateOne")
-//                .description("descriptionOne")
-//                .price(BigDecimal.valueOf(1000.50))
-//                .duration(10)
-//                .createDate(LocalDateTime.of(2022, 11, 20, 8, 44, 42, 8))
-//                .lastUpdateDate(LocalDateTime.of(2022, 11, 20, 8, 44, 42, 8))
-//                .tags(Set.of(new Tag("tag")))
-//                .build();
-//    private final GiftCertificate giftCertificateTwo = GiftCertificate.builder()
-//                .id(2L)
-//                .name("two certificate")
-//                .description("two description")
-//                .price(BigDecimal.valueOf(2000.50))
-//                .duration(20)
-//                .createDate(LocalDateTime.of(2022, 11, 21, 8, 44, 42, 8))
-//                .lastUpdateDate(LocalDateTime.of(2022, 11, 21, 8, 44, 42, 8))
-//                .tags(Set.of(new Tag("twoTag")))
-//                .build();
-//
-//    private final GiftCertificate giftCertificateThree = GiftCertificate.builder()
-//                .id(3L)
-//                .name("one more certificate")
-//                .description("more description")
-//                .price(BigDecimal.valueOf(3000.50))
-//                .duration(30)
-//                .createDate(LocalDateTime.of(2022, 11, 23, 8, 44, 42, 8))
-//                .lastUpdateDate(LocalDateTime.of(2022, 11, 23, 8, 44, 42, 8))
-//                .tags(Set.of(new Tag("fiveTag")))
-//                .build();
-//    private GiftCertificateRepository certificateRepository;
 //
 //    @Autowired
-//    public GiftCertificateRepositoryTest(GiftCertificateRepository certificateRepository) {
-//        this.certificateRepository = certificateRepository;
+//    private GiftCertificateRepository certificateRepository;
+//
+//    @Test
+//    void injectedComponentsAreNotNull(){
+//        assertThat(certificateRepository).isNotNull();
 //    }
+//
+////    @Autowired
+////    public GiftCertificateRepositoryTest(GiftCertificateRepository certificateRepository) {
+////        this.certificateRepository = certificateRepository;
+////    }
 //
 //    @Test
 //    void testFindAllCertificates() {
@@ -64,5 +40,10 @@
 //        assertEquals(expected, actual);
 //    }
 //
+//    @Test
+//    void testFindCertificateById() {
+//        Optional<GiftCertificate> giftCertificate = certificateRepository.findById(1L);
+//        assertNotNull(giftCertificate);
+//    }
 //
 //}
